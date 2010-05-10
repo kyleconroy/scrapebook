f9fbfe574e6a161cb358e88d73ed8d90cd9ab559
Kyle Conroy <kyle.j.conroy@gmail.com>
2010-05-10 12:43:29 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Scrapebook 
+
+## Because Facebook shouldn't own your soul
+
+Scrapebook uses Facebook's Graph API to download all your data from Facebook to your personal computer. Currently, the script only downloads photos, but I may add support for notes and statuses in the future. 
+
+### Usage
+
+Since the Graph API uses OAuth 2.0, this script requires an extra step. Log into Facebook and head over to <http://developers.facebook.com/docs/api>. Click on the "Photos: http://graph.facebook.com/me/photos" link, and copy the access_token variable from the address bar. Simply pass this value into the script as shown 
+
+    python scrapebook.py -t AUTH_TOKEN
+    
+and all your data will be downloaded into a folder titled "facebook" in the current directory
+
+**LONG LIVE DATA LIBERATION**
+
+
---
+++ scrapebook.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+#
+# The MIT License
+# 
+# Copyright (c) 2010 Kyle Conroy
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import os
+import sys
+import json
+import getopt
+import urllib2, urllib
+from multiprocessing import Process
+
+def save_photo(url, filename):
+        print "Saving" + filename
+        urllib.urlretrieve(url, filename)
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "t:h", ["token", "help"])
+    except getopt.GetoptError, err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+        
+    if not len(opts):
+        usage()
+        sys.exit(2)
+    
+    for o, a in opts:
+        if o in ("-t", "--token"):
+            token = a
+            scrape_photos(token)
+        else:
+            usage()
+
+def usage():
+    print """
+    python scrapebook.py -t AUTH_TOKEN
+    
+    To get your authtoken, head over to http://developers.facebook.com/docs/api,
+    click on the https://graph.facebook.com/me/photos link, and copy the auth
+    token in the url to the command line
+    """
+    
+def scrape_photos(token):
+    # Setup directory to store photos
+    scrape_dir = os.path.join(os.curdir,"facebook")
+    
+    try:
+        os.mkdir(scrape_dir)
+    except OSError:
+        # Folder already exists, continue on
+        pass
+    
+    limit = 10000 #Too big? Nah.....
+    url = "http://graph.facebook.com/me/photos?access_token=%s&limit=%d" % \
+        (token, limit)
+    data = urllib2.urlopen(url)
+    photos = json.loads(data.read())
+    
+    for i, photo in enumerate(photos["data"]):
+        purl = photo["source"]
+        filename = os.path.join(scrape_dir, "facebook_%s.jpg" % i)
+        p = Process(target=save_photo, args=(purl, filename))
+        p.start()
+
+if __name__ == '__main__':
+    main()
+
+    
