92ed9331f0de49951b6533a129b915d19256c2e6
Derferman <kyle.j.conroy@gmail.com>
2010-06-29 00:50:38 -0700

    Removed django dependency

--- scrapebook.py
+++ scrapebook.py
@@ -29,11 +29,23 @@ import sys
 import json
 import getopt
 import urllib2, urllib
+import re
+
 from multiprocessing import Pool
 
 # Need to remove this dependency 
-from django.template.defaultfilters import slugify
-
+try :
+    from django.template.defaultfilters import slugify
+except ImportError:
+    slugify = standalone_slugify
+    
+def standalone_slugify(inStr):
+    removelist = ["a", "an", "as", "at", "before", "but", "by", "for","from","is", "in", "into", "like", "of", "off", "on", "onto","per","since", "than", "the", "this", "that", "to", "up", "via","with"];
+    for a in removelist:
+        aslug = re.sub(r'\b'+a+r'\b','',inStr)
+    aslug = re.sub('[^\w\s-]', '', aslug).strip().lower()
+    aslug = re.sub('\s+', '-', aslug)
+    return aslug
 
 def usage():
     print """
